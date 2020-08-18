78bf1290ad4d8a58eee355729d5300f988cc7324
Prashant <prashant2381@gmail.com>
2020-08-18 21:42:30 +0530

    Added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,14 @@
-import { ProductDetailsGuard } from './shared/guards/product-detail.guard';
+import { ProductModule } from './products/product-list.component.module';
 import { WelcomeComponent } from './home/welcome.component';
-import { StarComponent } from './shared/components/star/star.component';
-import { ConvertHyphenToSpaces } from './shared/pipes/convert-hyphen-to-space.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ConvertHyphenToSpaces,
-    StarComponent,
-    ProductDetailsComponent,
-    WelcomeComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forChild([
-      {
-        path: 'products',
-        component: ProductListComponent,
-      },
-      {
-        path: 'products/:id',
-        component: ProductDetailsComponent,
-        canActivate: [ProductDetailsGuard],
-      },
-      {
-        path: 'welcome',
-        component: WelcomeComponent,
-      },
-      {
-        path: '',
-        redirectTo: 'welcome',
-        pathMatch: 'full',
-      },
-      {
-        path: '**',
-        redirectTo: 'welcome',
-        pathMatch: 'full',
-      },
-    ]),
-    FormsModule,
-    HttpClientModule,
-  ],
+  declarations: [AppComponent, WelcomeComponent],
+  imports: [BrowserModule, ProductModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/products/product-list.component.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './product-list.component';
+import { ConvertHyphenToSpaces } from '../shared/pipes/convert-hyphen-to-space.pipe';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailsGuard } from '../shared/guards/product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'products',
+        component: ProductListComponent,
+      },
+      {
+        path: 'products/:id',
+        component: ProductDetailsComponent,
+        canActivate: [ProductDetailsGuard],
+      },
+    ]),
+    SharedModule,
+  ],
+  declarations: [
+    ProductListComponent,
+    ConvertHyphenToSpaces,
+    ProductDetailsComponent,
+  ],
+})
+export class ProductModule {}
