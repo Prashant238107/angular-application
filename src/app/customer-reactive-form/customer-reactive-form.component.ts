bd9ccdff3f513517fc3390b1ce9b3481f082fb10
Prashant <prashant2381@gmail.com>
2020-08-20 05:56:43 +0530

    Added custom validation, custom validation with params

--- src/app/customer-reactive-form/customer-reactive-form.component.ts
+++ src/app/customer-reactive-form/customer-reactive-form.component.ts
@@ -1,7 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from '../shared/models/customer';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+} from '@angular/forms';
 
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    if (
+      (control.value !== null && isNaN(control.value)) ||
+      control.value < min ||
+      control.value > max
+    ) {
+      return { range: true };
+    }
+    return null;
+  };
+}
 @Component({
   selector: 'pm-customer-reactive-form',
   templateUrl: './customer-reactive-form.component.html',
@@ -15,10 +33,13 @@ export class CustomerReactiveFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.customerForm = this._formBuilder.group({
-      firstName: '',
-      lastName: { value: 'Verma', disabled: true },
-      email: '',
+      firstName: ['Prashant', [Validators.required, Validators.minLength(3)]],
+      lastName: ['Verma', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true,
+      rating: [null, [ratingRange(1, 5)]],
     });
   }
 
@@ -33,6 +54,9 @@ export class CustomerReactiveFormComponent implements OnInit {
       lastName: 'Verma',
       sendCatalog: true,
       email: 'prashant2381@gmail.com',
+      phone: '+91-9897158251',
+      notification: 'email',
+      rating: 5,
     });
   }
 
@@ -40,5 +64,16 @@ export class CustomerReactiveFormComponent implements OnInit {
     this.customerForm.patchValue({
       sendCatalog: false,
     });
+  }
+
+  setNotification(type: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (type === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
   }
 }
